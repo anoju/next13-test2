24fbad18c4ccb3c8a562a92265924f3f86b6fb9c
anoju <zun5761@gmail.com>
2023-10-18 13:32:10 +0900

    생활코딩 1-9

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello. Web
+      <br />
+      <br />
+      <img src="/elephant.png" />
+    </>
+  )
+}
